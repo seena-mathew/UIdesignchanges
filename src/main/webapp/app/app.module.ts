6a3611b91abec33a3de43616f959deea69ddd860
seena-mathew <seenamathew078@gmail.com>
2021-01-13 14:04:22 +0100

    Incorporated sidebar

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -7,6 +7,7 @@ import { UIdesignchangesCoreModule } from 'app/core/core.module';
 import { UIdesignchangesAppRoutingModule } from './app-routing.module';
 import { UIdesignchangesHomeModule } from './home/home.module';
 import { UIdesignchangesEntityModule } from './entities/entity.module';
+import { SidebarComponent } from 'app/layouts/sidebar/sidebar.component';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import { MainComponent } from './layouts/main/main.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
@@ -24,7 +25,7 @@ import { ErrorComponent } from './layouts/error/error.component';
     UIdesignchangesEntityModule,
     UIdesignchangesAppRoutingModule,
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent, SidebarComponent],
   bootstrap: [MainComponent],
 })
 export class UIdesignchangesAppModule {}
---
+++ src/main/webapp/app/layouts/sidebar/sidebar.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { VERSION } from 'app/app.constants';
+import { LoginService } from 'app/core/login/login.service';
+import { AccountService } from 'app/core/auth/account.service';
+import { LoginModalService } from 'app/core/login/login-modal.service';
+import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { Router } from '@angular/router';
+
+export interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+
+@Component({
+  moduleId: module.id,
+  selector: 'jhi-sidebar',
+  templateUrl: 'sidebar.component.html',
+  styleUrls: ['sidebar.scss'],
+})
+export class SidebarComponent implements OnInit {
+  public menuItems: any[] | undefined;
+  version: String;
+  inProduction?: boolean;
+  isNavbarCollapsed = false;
+  swaggerEnabled?: boolean;
+
+  constructor(
+    private loginService: LoginService,
+    private accountService: AccountService,
+    private loginModalService: LoginModalService,
+    private profileService: ProfileService,
+    private router: Router
+  ) {
+    this.version = VERSION ? (VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION) : '';
+  }
+
+  ngOnInit(): void {
+    this.profileService.getProfileInfo().subscribe(profileInfo => {
+      this.inProduction = profileInfo.inProduction;
+      this.swaggerEnabled = profileInfo.swaggerEnabled;
+    });
+  }
+
+  collapseNavbar(): void {
+    this.isNavbarCollapsed = true;
+  }
+
+  isAuthenticated(): boolean {
+    return this.accountService.isAuthenticated();
+  }
+
+  login(): void {
+    this.loginModalService.open();
+  }
+
+  logout(): void {
+    this.collapseNavbar();
+    this.loginService.logout();
+    this.router.navigate(['']);
+  }
+
+  toggleNavbar(): void {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  getImageUrl(): string {
+    return this.isAuthenticated() ? this.accountService.getImageUrl() : '';
+  }
+}
